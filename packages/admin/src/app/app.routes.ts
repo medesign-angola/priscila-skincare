712685fe57aa86856abc9715c3cef005f94d2cbc
IsaquiasMarques <patisaquias2000@gmail.com>
2026-09-08 14:39:11 +0100

    feat(admin): implement Figma login screen

--- packages/admin/src/app/app.routes.ts
+++ packages/admin/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { Login } from './pages/login';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: Login },
+  { path: '**', redirectTo: 'login' },
+];
--- packages/admin/src/app/app.ts
+++ packages/admin/src/app/app.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
-
-import { TranslatePipe } from '@ngx-translate/core';
-import { NgxMaskDirective } from 'ngx-mask';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [NxWelcome, RouterModule, TranslatePipe, NgxMaskDirective],
+  imports: [RouterOutlet],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected title = 'admin';
 }
